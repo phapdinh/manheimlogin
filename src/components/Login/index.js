ae925153217ad0da536f22b3cb4e48f8486a9946
Phap <centralparkloop@yahoo.com>
2019-04-25 17:21:10 -0400

    added some tests

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -16,13 +16,13 @@ class Login extends React.Component {
     render() {
         const { onSubmit, errorLogin } = this.props;
 
-        return <form>
+        return <form className="form">
             <input type="text" value={this.state.username} placeholder="Username" onChange={this.handleUserName} />
             <br />
             <input type="password" value={this.state.password} placeholder="Password" onChange={this.handlePassword} />
             <br />
             {errorLogin && <div className="alert_error">Login Failure</div>}
-            <button type="button" onClick={() => onSubmit(this.state.username, this.state.password)}>Sign In</button>
+            <button type="button" onClick={() => onSubmit(this.state.username, this.state.password)} className="button">Sign In</button>
         </form>
     }
 }
