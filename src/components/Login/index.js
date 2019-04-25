fe1a83eb2f4654ce9d9c1f79c1cc2f376df9c037
Phap <centralparkloop@yahoo.com>
2019-04-25 18:17:29 -0400

    align form

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -25,9 +25,7 @@ class Login extends React.Component {
 
         return <form className="form">
             <input type="text" value={this.state.username} placeholder="Username" onChange={this.handleUserName} />
-            <br />
             <input type="password" value={this.state.password} placeholder="Password" onChange={this.handlePassword} />
-            <br />
             {errorLogin && <div className="alert_error">Login Failure</div>}
             <button type="button" onClick={() => onSubmit(this.state.username, this.state.password)} className="button">Sign In</button>
         </form>
